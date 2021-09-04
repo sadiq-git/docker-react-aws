e3806f66b44f8a2d8be065cda20262b615e6262a
sadiqmuhamed <karurndl@gmail.com>
2021-09-04 17:27:56 +0530

    travis aws4 expose added 80

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi 2021 BYE
+          Docker - Git - TravisCI - AWSElasticBeanstalk
         </p>
         <a
           className="App-link"
